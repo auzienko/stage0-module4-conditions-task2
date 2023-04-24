2b22f16f85286ca426b9f4b77d567e38a7e9bb8a
Aleksandr Uzienko <a.uzienko@gmail.com>
2023-04-24 16:22:40 +0300

    solution

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,6 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-
+        System.out.println(500 <= from1UpTo1000 ? "Tail" : "Eagle");
     }
 }
